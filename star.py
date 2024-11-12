fef1e5f6aa82f81d9609b50e8fe58989753562fc
Rishik1507 <microsoftrishik@gmail.com>
2024-11-12 21:59:40 +0530

    c

---
+++ star.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+sc=turtle.Screen()
+sc.setup(400,400)
+turtle.title("Hello World!!")
+board=turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.penup()
+board.right(150)
+board.forward(50)
+board.pendown()
+board.right(90)
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+
+
+  
+turtle.done()
